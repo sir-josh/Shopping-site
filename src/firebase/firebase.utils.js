5a029ac3b7f3113e997d2ebd2894bfc1b8059274
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-09-03 05:18:53 +0100

    Create user context & refactor App.js to use context provider

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyDFu7YgMs-BogprQGekDjaSw4RX9M45exo",
+  authDomain: "crwn-db-551d2.firebaseapp.com",
+  databaseURL: "https://crwn-db-551d2.firebaseio.com",
+  projectId: "crwn-db-551d2",
+  storageBucket: "crwn-db-551d2.appspot.com",
+  messagingSenderId: "373423983592",
+  appId: "1:373423983592:web:ec05c92675f313113c325d",
+  measurementId: "G-N0SYH9S5C3"
 };
 
 firebase.initializeApp(config);
