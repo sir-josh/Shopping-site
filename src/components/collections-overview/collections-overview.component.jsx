f6e039c7480303b79d6bccb1a8d5a7bab2591631
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-09-04 11:26:48 +0100

    Resolve bug on the collections-overview component

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
-
 import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = () => {
-  const collections = useContext(CollectionsContext);
-
-  return (
+  const collectionsObj = useContext(CollectionsContext);
+  const collections = Object.keys(collectionsObj).map(key => collectionsObj[key]); // This convert the obj to an array
+  
+  return(
     <div className='collections-overview'>
       {collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
