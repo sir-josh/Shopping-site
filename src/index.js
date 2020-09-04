5c76af588999672854b2fe3efe2e612f08b65a2f
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2020-09-04 12:00:17 +0100

    Remove instances of redux implementation from the application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-
-import { store, persistor } from './redux/store';
 import CartProvider from './providers/cart/cart.provider';
 
 import './index.css';
@@ -12,13 +8,9 @@ import App from './App';
 
 ReactDOM.render(
   <CartProvider>
-    <Provider store={store}>
       <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
+          <App /> 
       </BrowserRouter>
-    </Provider>
   </CartProvider>,
   document.getElementById('root')
 );
